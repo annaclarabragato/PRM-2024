d101cc97a07d70cdd776d44ecb55a2ff7a18d4e2
annaclarabragato <annaclarabragato@gmail.com>
2024-10-10 22:04:54 -0300

    aula 10/10

--- mater-play-backend/src/entities/movie-entity.ts
+++ mater-play-backend/src/entities/movie-entity.ts
@@ -1,7 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('movie')
 export class Movie {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ nullable: false })
   title: string;
+
+  @Column({ type: 'text', nullable: false })
   description: string;
+
   ageRating: string;
+
+  @Column({ nullable: false })
   poster: string;
 }
