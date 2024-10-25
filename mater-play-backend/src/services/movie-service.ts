7daaea146cbe19f0ef03de85aa3d32af0ab531ee
annaclarabragato <annaclarabragato@gmail.com>
2024-10-24 21:58:47 -0300

    aula 24/10

--- mater-play-backend/src/services/movie-service.ts
+++ mater-play-backend/src/services/movie-service.ts
@@ -14,7 +14,16 @@ export class MovieService {
         return this.repository.find();
     }
 
-findById(id: string): Promise<Movie> {
-    return this.repository.findOneBy({id: id});
-}
+    findById(id: string): Promise<Movie> {
+        return this.repository.findOneBy({ id: id });
+    }
+
+    save(movie: Movie): Promise<Movie> {
+        return this.repository.save(movie);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.repository.delete(id);
+    }
+
 }
